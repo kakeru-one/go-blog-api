0f433178ccaf9f693f9bb2e7e358a6030155a75b
ochi-sho-private-study <ochisyoji1118@gmail.com>
2024-07-23 21:02:54 +0900

    1.2 handlersを別パッケージにする

--- api/main.go
+++ api/main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
-		"io"
-		"log"
-		"net/http"
+	"log"
+	"net/http"
+
+	"github.com/ochi-sho-private-study/go-blog-api/handlers"
 )
 
 func main() {
-		helloHandler := func(w http.ResponseWriter, req *http.Request) {
-        io.WriteString(w, "Hello, world!\n")
-    }
+	http.HandleFunc("/hello", handlers.HelloHandler)
+	http.HandleFunc("/article", handlers.PostArticleHandler)
+	http.HandleFunc("/article/list", handlers.ArticleListHandler)
+	http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
+	http.HandleFunc("/article/nice", handlers.PostNiceHandler)
+	http.HandleFunc("/comment", handlers.PostCommentHandler)
 
-    http.HandleFunc("/", helloHandler)
-
-    log.Println("server start at port 8080")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Println("server start at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
